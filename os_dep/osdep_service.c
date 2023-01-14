1720016d26bae721cb49387063890dfc9411aa4a
KanuX-14 <kanux.dev@gmail.com>
2023-01-14 19:42:56 -0300

    Build for kernel 6.1.4+

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -2571,13 +2571,15 @@ u64 rtw_division64(u64 x, u64 y)
 inline u32 rtw_random32(void)
 {
 #ifdef PLATFORM_LINUX
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
+	u32 random_int;
+	get_random_bytes(&random_int , 4);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 4))
+	return get_random_u32_below(random_int);
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
 	return prandom_u32_max(0);
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0))
 	return prandom_u32();
 #elif (LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18))
-	u32 random_int;
-	get_random_bytes(&random_int , 4);
 	return random_int;
 #else
 	return random32();
