0b0f0ceb6d98b4128888f05dde1263b7fd53f7b8
KanuX-14 <kanux.dev@gmail.com>
2023-05-18 11:50:38 -0300

    Build script remaped
    Recent issues pointed that the old build script is unforeseeable, as it can break easily.
    
    From the changes it includes:
    * Better root check;
    * Root manager checker. If no root manager is provided, "su -c" will be used;
    * Better package manager check;
    * Better kernel version check;
    * More comments. Comments above functions provide enough information to understand what it does;
    * "/usr/local/sbin/" changed to "/usr/local/bin/";
    * The repository will now be successfully checked and used. Before it was very bugged;
    * If no repository is found, it will create a "git" folder under "$HOME/.local/share/" folder with the repository folder inside;
    * "rmmod" changed to "modprobe -r";
    * The script will actually check if the driver is installed correctly.
    
    Double-check of the driver installation will be provided later.
    This version is being posted before the implementation because it is very stable compared to it's old script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,53 +1,16 @@
 #!/bin/bash
 
-# Check kernel version.
-function check_if_new()
-{
-  local KERNEL_VERSION=$(uname --kernel-release | cut --delimiter="." --fields=1-2)
-  local KERNEL_MAJOR_VERSION=$(echo ${KERNEL_VERSION} | cut --delimiter="." --fields=1)
-  local KERNEL_MINOR_VERSION=$(echo ${KERNEL_VERSION} | cut --delimiter="." --fields=2)
+### Global variables ###
 
-  if [ ${KERNEL_MAJOR_VERSION} -ge "6" ]; then
-    if [ ${KERNEL_MINOR_VERSION} -ge "3" ]; then
-      return 0
-    else
-      return 1
-    fi
-  else
-    return 1
-  fi
-}
-
-# Apply driver name based on the kernel version.
-CURRENT_DRIVER=""
-if check_if_new; then
-  CURRENT_DRIVER="rtl8xxxu"
-else
-  CURRENT_DRIVER="r8188eu"
-fi
-NEW_DRIVER="8188eu"
-
-# Check the package manager.
-if type dpkg &>/dev/null; then
-	sudo apt-get update && sudo apt-get install gawk tar git gcc bc make linux-headers-$(uname -r) zenity -y
-elif type pacman &>/dev/null; then
-	sudo pacman -S --needed --noconfirm gawk tar git gcc bc make linux-headers zenity
-elif type dnf &>/dev/null; then
-	sudo dnf install -y gawk tar git gcc bc make kernel-devel zenity
-else
-	printf "${GREEN}""Consider installing the kernel headers by yourself.""${RESET_COLOUR}""\n"
-  exit 1
-fi
-
-# Environment variables
-CURRENT_SHELL=$(ps -hp $$ | awk '{printf $5}')
-CURRENT_USER=$(whoami)
-CURRENT_HOME=""
-CURRENT_PWD=$(pwd)
-CORE=$(nproc)
-IS_NOT_BLACKLISTED=$(cat /etc/modprobe.d/realtek.conf | grep "blacklist ${CURRENT_DRIVER}")
-INSTALLATION_PATH_DOES_NOT_EXIST=$(printf $PATH | grep /usr/local/sbin)
-DO_DIRECTORY_EXISTS=0
+CURRENT_KERNEL=$(uname --kernel-release)
+ROOT=""
+PM=""
+PM_ATTRIBUTES=""
+DEPENDENCIES=""
+COMMON_DEPENDENCIES="gawk tar git gcc bc make zenity"
+UPDATE_PROCEDURE=""
+INSTALL_PROCEDURE=""
+REMOVE_PROCEDURE="" # Can break packages. Will not use unless necessary.
 
 # Colours
 RED="\e[1;31m"
@@ -58,43 +21,179 @@ PURPLE="\e[1;35m"
 CYAN="\e[1;36m"
 RESET_COLOUR="\e[0m"
 
-# Set home based on user or root
-if [ ${CURRENT_USER} == "root" ]; then
-	CURRENT_HOME="/root"
-else
-	CURRENT_HOME="/home/${CURRENT_USER}"
+### Functions ###
+
+# int: Checks if script is running with root.
+#      If not, no updates will happen.
+#      The script will compile but will not install.
+function AM_I_ROOT()
+{
+  local user=$(whoami)
+
+  case $user in
+  root) return 0 ;;
+  *) return 1 ;;
+  esac
+}
+
+# void: Get the root manager name.
+#       If no root manager is found, "sh -c" will be used.
+#       Therefore the user will need to insert the password for every
+#         root dependable command.
+function GET_ROOT_MANAGER()
+{
+  if type sudo &>/dev/null; then ROOT="sudo"
+  elif type doas &>/dev/null; then ROOT="doas"
+  else ROOT="su -c"
+  fi
+}
+
+# int: Check kernel version.
+function CHECK_KERNEL_VERSION()
+{
+  local KERNEL_VERSION=$(printf "${CURRENT_KERNEL}" | cut --delimiter="." --fields=1-2)
+  local KERNEL_MAJOR_VERSION=$(printf "${KERNEL_VERSION}" | cut --delimiter="." --fields=1)
+  local KERNEL_MINOR_VERSION=$(printf "${KERNEL_VERSION}" | cut --delimiter="." --fields=2)
+
+  # If kernel is greater or equal to 6.3.x.
+  if [ ${KERNEL_MAJOR_VERSION} -ge "6" ]
+    then if [ ${KERNEL_MINOR_VERSION} -ge "3" ]
+      then return 0
+      else return 1
+    fi
+  else return 1
+  fi
+}
+
+# int: Return the package manager name.
+function CHECK_PACKAGE_MANAGER()
+{
+  if type dpkg &>/dev/null; then return 1
+  elif type pacman &>/dev/null; then return 2
+  elif type dnf &>/dev/null; then return 3
+  else return 0
+  fi
+}
+
+# void: Get the package manager information.
+function GET_PM_INFO()
+{
+  CHECK_PACKAGE_MANAGER
+  local i=$?
+
+  case $i in
+  0)
+    printf "${YELLOW}""Warning: Package manager not found. Skipping dependency check...""${RESET_COLOUR}""\n"
+    exit 1
+    ;;
+  1)
+    PM="apt-get"
+    PM_ATTRIBUTES="-y"
+    UPDATE_PROCEDURE="update"
+    INSTALL_PROCEDURE="install"
+    REMOVE_PROCEDURE="--purge remove"
+    DEPENDENCIES="linux-headers-${CURRENT_KERNEL}"
+    ;;
+  2)
+    PM="pacman"
+    PM_ATTRIBUTES="--needed --noconfirm"
+    UPDATE_PROCEDURE="-Sy"
+    INSTALL_PROCEDURE="-S"
+    REMOVE_PROCEDURE="-Rdd"
+    DEPENDENCIES="linux-headers"
+    ;;
+  3)
+    PM="dnf"
+    PM_ATTRIBUTES="-y"
+    UPDATE_PROCEDURE="check-update"
+    INSTALL_PROCEDURE="install"
+    DEPENDENCIES="kernel-devel"
+    ;;
+  esac
+}
+
+### Check about root ###
+
+CURRENT_HOME=""
+if ! AM_I_ROOT
+  then
+    printf "${YELLOW}""Warning: Script running without root privileges. If no recognized root manager is found, the script will use \"su -c\" instead...""${RESET_COLOUR}""\n"
+    GET_ROOT_MANAGER
+    CURRENT_HOME="/home/$(whoami)/"
+  else CURRENT_HOME="/root/"
 fi
 
-# Check if the repository already exists
-if ! [ -d ../rtl8188eus ]; then
-	if ! [ -d ${CURRENT_HOME}/rtl8188eus ]; then
-		git clone --recursive https://gitlab.com/KanuX/rtl8188eus.git
-	else
-		printf "${YELLOW}""[!] Repository already exists.""${RESET_COLOUR}""\n"
-	fi
-	cd "${CURRENT_HOME}/rtl8188eus"
+### Find about package availability ###
+
+# Apply driver name based on the kernel version.
+CURRENT_DRIVER=""
+if CHECK_KERNEL_VERSION
+  then CURRENT_DRIVER="rtl8xxxu"
+  else CURRENT_DRIVER="r8188eu"
 fi
+NEW_DRIVER="8188eu"
+
+# Get package manager information.
+GET_PM_INFO
+
+# Update and install provided packages.
+${ROOT} ${PM} ${UPDATE_PROCEDURE} && ${ROOT} ${PM} ${PM_ATTRIBUTES} ${INSTALL_PROCEDURE} ${DEPENDENCIES} ${COMMON_DEPENDENCIES}
+
+### Proceed with the compiling ### 
+
+# Environment variables.
+CURRENT_SHELL=$(ps -hp $$ | awk '{printf $5}')
+CURRENT_PWD=$(pwd)
+CORE=$(nproc)
+IS_NOT_BLACKLISTED=$(cat /etc/modprobe.d/realtek.conf | grep "blacklist ${CURRENT_DRIVER}")
+INSTALLATION_PATH_DOES_NOT_EXIST=$(printf $PATH | grep /usr/local/bin)
+DO_DIRECTORY_EXISTS=0
+REPOSITORY_HOME="../rtl8188eus/"
+
+# Check if the repository already exists.
+if ! [ -d ${REPOSITORY_HOME} ]
+  then if ! [ -d ${CURRENT_HOME}/.local/share/git/rtl8188eus ]
+    then
+      REPOSITORY_HOME="${CURRENT_HOME}/.local/share/git/rtl8188eus/"
+      mkdir -pv ${REPOSITORY_HOME}
+      git clone --recursive https://gitlab.com/KanuX/rtl8188eus.git "${REPOSITORY_HOME}"
+    else
+      printf "${YELLOW}""Warning: Repository already exists.""${RESET_COLOUR}""\n"
+      REPOSITORY_HOME="${CURRENT_HOME}/.local/share/git/rtl8188eus/"
+  fi
+  else printf "${YELLOW}""Warning: Repository already exists.""${RESET_COLOUR}""\n"
+fi
+cd ${REPOSITORY_HOME}
 
 # Check if the device is already blacklisted.
 if [ -z "${IS_NOT_BLACKLISTED}" ]; then
-	sudo mkdir -pv /etc/modprobe.d
-	printf "blacklist ${CURRENT_DRIVER}\n" | sudo tee "/etc/modprobe.d/realtek.conf"
+  ${ROOT} mkdir -pv /etc/modprobe.d/
+  printf "blacklist ${CURRENT_DRIVER}\n" | ${ROOT} tee /etc/modprobe.d/realtek.conf
 fi
 
-# Check if /usr/local/sbin exists.
+# Check if /usr/local/bin/ exists.
 if [ -z "${INSTALLATION_PATH_DOES_NOT_EXIST}" ]; then
-	sudo mkdir -pv /usr/local/sbin
-	printf "export PATH=/usr/local/sbin:$PATH\n" >> ."${CURRENT_SHELL}""rc"
+  ${ROOT} mkdir -pv /usr/local/bin/
+  printf "export PATH=/usr/local/bin:$PATH\n" >> ${CURRENT_HOME}/."${CURRENT_SHELL}""rc"
 fi
 
 # Installation process.
-sudo rmmod ${CURRENT_DRIVER} # This one can be anything. I am using this one because is the common TL-WN722N v2/v3
-sudo rmmod ${NEW_DRIVER} 2&>/dev/null # Remove old/installed module. Visual purposes.
-make -j${CORE} && sudo make install
-sudo modprobe ${NEW_DRIVER} # Load the new driver/module
-sudo cp -v scripts/toggle-monitor.sh /usr/local/bin/toggle-monitor
-sudo chown ${USER}:${USER} /usr/local/bin/toggle-monitor
-sudo chmod +x /usr/local/bin/toggle-monitor
-sudo cp -v rtl8188eus-toggle-monitor.desktop /usr/share/applications
-printf "${GREEN}""The driver/module has been installed!""${RESET_COLOUR}""\n"
-notify-send "The driver/module has been installed!"
+${ROOT} modprobe -r ${CURRENT_DRIVER} # This one can be anything. I am using this one because is the common TL-WN722N v2/v3
+${ROOT} modprobe -r ${NEW_DRIVER} 2&>/dev/null # Remove old/installed module. Visual purposes.
+make -j${CORE} && ${ROOT} make install
+${ROOT} modprobe ${NEW_DRIVER} # Load the new driver/module
+${ROOT} cp -v scripts/toggle-monitor.sh /usr/local/bin/toggle-monitor
+${ROOT} chown ${USER}:${USER} /usr/local/bin/toggle-monitor
+${ROOT} chmod +x /usr/local/bin/toggle-monitor
+${ROOT} cp -v rtl8188eus-toggle-monitor.desktop /usr/share/applications
+
+# Check if compiled and installed.
+if ! ls /usr/lib/modules/${CURRENT_KERNEL}/kernel/drivers/net/wireless/ | grep ${NEW_DRIVER}
+  then
+    printf "${RED}""The driver could not be installed. Please post the log under the \"issues\" tab.""${RESET_COLOUR}""\n"
+    exit 1
+  else
+    printf "${GREEN}""The driver/module has been installed!""${RESET_COLOUR}""\n"
+    notify-send "The driver/module has been installed!"
+    exit 0
+fi
