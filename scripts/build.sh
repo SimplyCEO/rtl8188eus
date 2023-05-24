bf425a9751d37f85da1091e01678e89605c794a2
KanuX-14 <kanux.dev@gmail.com>
2023-05-24 10:23:02 -0300

    Changed colour calling from 'e' to 'x1b'
    Matches ANSI C colouring.

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,13 +13,13 @@ INSTALL_PROCEDURE=""
 REMOVE_PROCEDURE="" # Can break packages. Will not use unless necessary.
 
 # Colours
-RED="\e[1;31m"
-GREEN="\e[1;32m"
-YELLOW="\e[1;33m"
-BLUE="\e[1;34m"
-PURPLE="\e[1;35m"
-CYAN="\e[1;36m"
-RESET_COLOUR="\e[0m"
+RED="\x1b[1;31m"
+GREEN="\x1b[1;32m"
+YELLOW="\x1b[1;33m"
+BLUE="\x1b[1;34m"
+PURPLE="\x1b[1;35m"
+CYAN="\x1b[1;36m"
+RESET_COLOUR="\x1b[0m"
 
 ### Functions ###
 
